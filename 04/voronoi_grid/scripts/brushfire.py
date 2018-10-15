a44571adc58c683a086f5737552e8441e147eb67
Paul-Louis Azra <Paul@patherceve.local>
2018-10-15 15:26:24 -0400

    brushfire II

--- 04/voronoi_grid/scripts/brushfire.py
+++ 04/voronoi_grid/scripts/brushfire.py
@@ -2,13 +2,13 @@
 
 import rospy
 from voronoi_grid.msg import GridInfo
+from nav_msgs.msg import OccupancyGrid
+import time
 
 gridInfo = GridInfo()
 
 def gridInfoCb(gridInfo):
 	print 'In gridInfoCb'
-	#print gridInfo.grid.data
-
 	brushfire(gridInfo)
 
 def oneDToTwo(i, width):
@@ -17,60 +17,107 @@ def oneDToTwo(i, width):
 def twoDToOne(r, c, width):
 	return (r * width) + c
 
-def getNeighbors(i, width):
+def inLine(grid):
+	outGrid = OccupancyGrid()
+	for i_r, r in enumerate(grid):
+		for j_c, c in enumerate(r):
+			outGrid.data.append(c)
 
-	row, col = oneDToTwo(i, width)
+	outGrid.header.frame_id = 'map'
+	outGrid.info.width = 100
+	outGrid.info.height = 100
+	outGrid.info.resolution = 0.05
+	return outGrid
 
-	if(row > 0):
-		up = twoDToOne(row - 1, col, width)
-	else:
-		up = -1
-
-	if(row < width - 1):
-		down = twoDToOne(row + 1, col, width)
-	else:
-		down = -1
-
-	if(col > 0):
-		left = twoDToOne(row, col - 1, width)
-	else:
-		left = -1
-
-	if(col < width - 1):
-		right = twoDToOne(row, col + 1, width)
-	else:
-		right = -1
-
-	return [up, down, left, right]
-	
+def getNeighbors2D(row, col):
+	# up, down, left, right
+	return [[row-1, col], [row+1, col], [row, col-1], [row, col+1]]
 
 def brushfire(gridInfo):
 	print 'In brushfire'
 
-	#print gridInfo.grid
-	#print gridInfo.obs
+	pub = rospy.Publisher('/bf_map', OccupancyGrid, queue_size=100)
 
+	# getting the width of the map
 	width = gridInfo.grid.info.width
-	#print "width: %s" % width
 
-	for i in range(0, len(gridInfo.grid.data)):
+	# making an 2D list of 0
+	brushGrid = [[0]*width for _ in xrange(width)]
+	
+	# setting the edges of the map to 2
+	for i in [0, width-1]:
+		brushGrid[i] = [2]*width
+	for i in range(1, width-1):
+		brushGrid[i] = [2]+([0]*(width-2))+[2]
 
-		#print i
+	# setting the obstacle to 1 and the obstacle's neighbors to 2
+	for obstacle in range(0, len(gridInfo.obs)):
+		for p in gridInfo.obs[obstacle].i_pixels:
+			r, c = oneDToTwo(p, width)
+			brushGrid[r][c] = 1
+		# coordinates of the top left corner
+		tlR, tlC = oneDToTwo(gridInfo.obs[obstacle].i_pixels[0], width)
+		# coordinates of the bottom right corner
+		brR, brC = oneDToTwo(gridInfo.obs[obstacle].i_pixels[len(gridInfo.obs[obstacle].i_pixels)-1], width)
+		# print 'top left: r: %s c: %s' %(tlR, tlC)
+		# print 'bottom right: r: %s c: %s' %(brR, brC)
+		for obsCol in range(tlC, brC+1):
+			brushGrid[tlR-1][obsCol] = 2
+			brushGrid[brR+1][obsCol] = 2
+		for obsRow in range(tlR, brR+1):
+			brushGrid[obsRow][tlC-1] = 2
+			brushGrid[obsRow][brC+1] = 2
+	
+	# listing all non 0 locations
+	nonZeroLocations = list()
+	for r in range(0, width):
+		for c in range(0, width):
+			if not(brushGrid[r][c] == 0):
+				nonZeroLocations.append([r, c])
 
-		# row = i / width
-		# col = i % width
+#
+# WORK ZONE
+#
 
-		neighbors = getNeighbors(i, width)
+	# setting count for distance value
+	count = 2
+	# setting the grid on fire
+	while not(len(nonZeroLocations) == 0):
+		print 'In while'
+		for e in nonZeroLocations:
+			print 'In nonZeroLocations'
+			current = nonZeroLocations.pop(nonZeroLocations.index(e))
+			# setting the neighbors list
+			neighbors = list()
+			neighbors = getNeighbors2D(current[0], current[1])
+			print 'len(neighbors): %s' % len(neighbors)
+			# finding neighbors with count value i, and assining i+1 to current location
+			for n in neighbors:
+				# n[o]>-1 and < height and n[1]...
+				if(brushGrid[n[0]][n[1]] == 0):
+					brushGrid[n[0]][n[1]] = count + 1
 
-		# print "up, down, left, right: %s" % neighbors
+		count += 1
 
+#
+# END WORK ZONE
+#
 
-		# make an array of all zeros, set the obstacles, then set brushfire
-		for n in neighbors:
-			if gridInfo.grid.data[n] == 0:
-				gridInfo.grid.data[n] += 1
+	pub.publish(inLine(brushGrid))
+	pub.publish(inLine(brushGrid))
+	time.sleep(1)
+	pub.publish(inLine(brushGrid))
+	pub.publish(inLine(brushGrid))
+	time.sleep(1)
+	pub.publish(inLine(brushGrid))
+	pub.publish(inLine(brushGrid))
+	time.sleep(1)
 
-	print gridInfo.grid
+	# # output to file
+	# o = open('out.txt', 'w')
+	# for e in brushGrid:
+	# 	o.write(' '.join(map(str, e)) + '\n')
+	# o.close
 
 def main():
     print 'In main'
